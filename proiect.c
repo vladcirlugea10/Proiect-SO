85352e8573939ee71273fe80ae4a9ea509a28b1d
vladcirlugea10 <vladcirlugea@gmail.com>
2023-11-13 13:51:59 +0200

    Saptamana 7

--- proiect.c
+++ proiect.c
@@ -7,7 +7,7 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <time.h>
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 512
 #define READ_BUFFER_SIZE 2
 
 int verificareArgument(int nrArgumente, char* director){
@@ -82,70 +82,40 @@ int creareFisier(char *denumire){
     }
     else
     {
-        printf("Fisier de iesire creat!\n");
+        printf("Fisier creat!\n");
     }
     return fisier;
 }
 
-void scriereDataBMP(int fisierIntrare, int fisierIesire){
-    int count = 0;
+void scriereDataBMP(int fisierIntrare, int fisierIesire) {
     char buffer[BUFFER_SIZE];
-    char *fileSize, *fileWidth, *fileHeight;
-    fileSize = malloc(BUFFER_SIZE * sizeof(char));
-    fileWidth = malloc(BUFFER_SIZE * sizeof(char));
-    fileHeight = malloc(BUFFER_SIZE * sizeof(char));
-    if(fileSize == NULL || fileWidth == NULL || fileHeight == NULL) {
-        perror("Failed to allocate memory");
+
+    if (read(fisierIntrare, buffer, 54) != 54) {
+        perror("Eroare la citirea header-ului BMP!\n");
         exit(-1);
     }
-    
-    while(read(fisierIntrare, buffer, READ_BUFFER_SIZE)> 0)
-    {
-        count = count + 2;
-        char asciiBuffer[READ_BUFFER_SIZE * 2 + 1];
-        for (int i = 0; i < READ_BUFFER_SIZE; i++) {
-            sprintf(asciiBuffer + i * 2, "%02X", buffer[i]);
-        }
-        if(count == 4)
-        {
-            sprintf(fileSize, "Dimensiune: %s\n", asciiBuffer);
-        }
-        if(count == 6){
-            strcat(fileSize, buffer);
-            if(write(fisierIesire, fileSize, strlen(fileSize)) < 0){
-                perror("Eroare la scrierea 'fileSize'!\n");
-                exit(-1);
-            }
-        }
-        if(count == 20){
-            sprintf(fileWidth, "Latime: %s\n", buffer);
-        }
-        if(count == 22){
-            strcat(fileWidth, buffer);
-            if(write(fisierIesire, fileWidth, strlen(fileWidth)) < 0){
-                perror("Eroare la scrierea 'fileWidth'!\n");
-                exit(-1);
-            }
-        }
-        if(count == 24){
-            sprintf(fileHeight, "Inaltime: %s\n", buffer);
-        }
-        if(count == 26){
-            strcat(fileHeight, buffer);
-            if(write(fisierIesire, fileHeight, strlen(fileHeight)) < 0){
-                perror("Eroare la scrierea 'fileHeight'!\n");
-                exit(-1);
-            }
-        }
-        if(count > 30){
-            break;
-        }
-    }
+    int width = *(int*)&buffer[18];
+    int height = *(int*)&buffer[22];
+    int size = *(int*)&buffer[2];
+
+    char widthString[BUFFER_SIZE];
+    char w[BUFFER_SIZE] = "Latime: ";
+    sprintf(widthString, "%d\n", width);
+    strcat(w, widthString);
+    char heightString[BUFFER_SIZE];
+    char h[BUFFER_SIZE] = "Inaltime: ";
+    sprintf(heightString, "%d\n", height);
+    strcat(h, heightString);
+    char sizeString[BUFFER_SIZE];
+    char s[BUFFER_SIZE] = "Dimensiune: ";
+    sprintf(sizeString, "%d octeti\n", size);
+    strcat(s, sizeString);
+
+    write(fisierIesire, s, strlen(s));
+    write(fisierIesire, h, strlen(h));
+    write(fisierIesire, w, strlen(w));
 
     printf("Scriere terminata\n");
-    free(fileHeight);
-    free(fileWidth);
-    free(fileSize);
 }
 
 void scriereDateFisier(int fisierIntrare, int fisierIesire){
@@ -238,7 +208,7 @@ void scriereDateFisier(int fisierIntrare, int fisierIesire){
         strcat(otherAccessRight, "-");
     }
     if(data.st_mode & S_IXOTH){
-        strcat(otherAccessRight, "X\n");
+        strcat(otherAccessRight, "X\n\n");
     }
     else{
         strcat(otherAccessRight, "-\n\n");
@@ -258,31 +228,241 @@ void scrieNumeFisier(int fisierIesire, char *denumireFisier){
     }
 }
 
-void parcurgeDirector(char * director, int *fisierIesire){
-    DIR *dir;
-    struct dirent *intrare;
+void scrieLegaturaSimbolica(int fisierIesire, char *legatura, char *fisierTarget, char *director) {
+    char linkName[BUFFER_SIZE];
+    char linkSize[BUFFER_SIZE];
+    char targetSize[BUFFER_SIZE];
+    int fTarget = 0;
 
-    if((dir = opendir(director)) == NULL){
-        perror("Eroare la deschiderea directorului!\n");
+    sprintf(linkName, "Nume legatura: %s\n", legatura);
+    char caleCompleta[BUFFER_SIZE];
+    sprintf(caleCompleta, "%s/%s", director, fisierTarget);
+
+    if((fTarget = open(caleCompleta, O_RDONLY)) < 0){
+        perror("Eroare la deschiderea fisierului target!\n");
         exit(-1);
     }
 
-    printf("Deschidere director cu succes: %s\n", director);
+    struct stat linkStat, targetStat;
+    if (lstat(legatura, &linkStat) < 0) {
+        perror("Eroare la preluarea informatiilor despre legatura simbolica!\n");
+        exit(-1);
+    }
+    if (fstat(fTarget, &targetStat) < 0) {
+        perror("Eroare la preluarea informatiilor despre fisierul target!\n");
+        exit(-1);
+    }
+
+    sprintf(linkSize, "Dimensiune legatura: %d octeti\n", linkStat.st_size);
+    sprintf(targetSize, "Dimensiune fisier target: %d octeti\n", targetStat.st_size);
+
+    if (write(fisierIesire, linkName, strlen(linkName)) < 0) {
+        perror("Eroare la scrierea numelui legaturii!\n");
+        exit(-1);
+    }
+    if (write(fisierIesire, linkSize, strlen(linkSize)) < 0) {
+        perror("Eroare la scrierea dimensiunii legaturii!\n");
+        exit(-1);
+    }
+    if (write(fisierIesire, targetSize, strlen(targetSize)) < 0) {
+        perror("Eroare la scrierea dimensiunii fisierului target!\n");
+        exit(-1);
+    }
+
+    char userAccessRight[BUFFER_SIZE] = "Drepturi de acces user legatura: ";
+    char groupAccessRight[BUFFER_SIZE] = "Drepturi de acces grup legatura: ";
+    char otherAccessRight[BUFFER_SIZE] = "Drepturi de acces altii legatura: ";
+
+    if (linkStat.st_mode & S_IRUSR) {
+        strcat(userAccessRight, "R");
+    } else {
+        strcat(userAccessRight, "-");
+    }
+    if (linkStat.st_mode & S_IWUSR) {
+        strcat(userAccessRight, "W");
+    } else {
+        strcat(userAccessRight, "-");
+    }
+    if (linkStat.st_mode & S_IXUSR) {
+        strcat(userAccessRight, "X\n");
+    } else {
+        strcat(userAccessRight, "-\n");
+    }
+
+    if (write(fisierIesire, userAccessRight, strlen(userAccessRight)) < 0) {
+        perror("Eroare la scrierea drepturilor de acces user pentru legatura!\n");
+        exit(-1);
+    }
+
+    if (linkStat.st_mode & S_IRGRP) {
+        strcat(groupAccessRight, "R");
+    } else {
+        strcat(groupAccessRight, "-");
+    }
+    if (linkStat.st_mode & S_IWGRP) {
+        strcat(groupAccessRight, "W");
+    } else {
+        strcat(groupAccessRight, "-");
+    }
+    if (linkStat.st_mode & S_IXGRP) {
+        strcat(groupAccessRight, "X\n");
+    } else {
+        strcat(groupAccessRight, "-\n");
+    }
+
+    if (write(fisierIesire, groupAccessRight, strlen(groupAccessRight)) < 0) {
+        perror("Eroare la scrierea drepturilor de acces grup pentru legatura!\n");
+        exit(-1);
+    }
+
+    if (linkStat.st_mode & S_IROTH) {
+        strcat(otherAccessRight, "R");
+    } else {
+        strcat(otherAccessRight, "-");
+    }
+    if (linkStat.st_mode & S_IWOTH) {
+        strcat(otherAccessRight, "W");
+    } else {
+        strcat(otherAccessRight, "-");
+    }
+    if (linkStat.st_mode & S_IXOTH) {
+        strcat(otherAccessRight, "X\n\n");
+    } else {
+        strcat(otherAccessRight, "-\n\n");
+    }
+
+    if (write(fisierIesire, otherAccessRight, strlen(otherAccessRight)) < 0) {
+        perror("Eroare la scrierea drepturilor de acces altii pentru legatura!\n");
+        exit(-1);
+    }
+}
+
+void scrieDirector(int fisierIesire, char *director) {
+    char dirName[BUFFER_SIZE];
+    char userID[BUFFER_SIZE];
+    char userAccessRight[BUFFER_SIZE] = "Drepturi de acces user: ";
+    char groupAccessRight[BUFFER_SIZE] = "Drepturi de acces grup: ";
+    char otherAccessRight[BUFFER_SIZE] = "Drepturi de acces altii: ";
+
+    sprintf(dirName, "Nume director: %s\n", director);
+
+    struct stat dirStat;
+    if (stat(director, &dirStat) < 0) {
+        perror("Eroare la preluarea informatiilor despre director!\n");
+        exit(-1);
+    }
+
+    sprintf(userID, "Identificatorul Utilizatorului: %d\n", dirStat.st_uid);
+
+    if (write(fisierIesire, dirName, strlen(dirName)) < 0) {
+        perror("Eroare la scrierea numelui directorului!\n");
+        exit(-1);
+    }
+    if (write(fisierIesire, userID, strlen(userID)) < 0) {
+        perror("Eroare la scrierea identificatorului utilizatorului pentru director!\n");
+        exit(-1);
+    }
+
+    if (dirStat.st_mode & S_IRUSR) {
+        strcat(userAccessRight, "R");
+    } else {
+        strcat(userAccessRight, "-");
+    }
+    if (dirStat.st_mode & S_IWUSR) {
+        strcat(userAccessRight, "W");
+    } else {
+        strcat(userAccessRight, "-");
+    }
+    if (dirStat.st_mode & S_IXUSR) {
+        strcat(userAccessRight, "X\n");
+    } else {
+        strcat(userAccessRight, "-\n");
+    }
+    if (write(fisierIesire, userAccessRight, strlen(userAccessRight)) < 0) {
+        perror("Eroare la scrierea drepturilor de acces user pentru director!\n");
+        exit(-1);
+    }
+
+    if (dirStat.st_mode & S_IRGRP) {
+        strcat(groupAccessRight, "R");
+    } else {
+        strcat(groupAccessRight, "-");
+    }
+    if (dirStat.st_mode & S_IWGRP) {
+        strcat(groupAccessRight, "W");
+    } else {
+        strcat(groupAccessRight, "-");
+    }
+    if (dirStat.st_mode & S_IXGRP) {
+        strcat(groupAccessRight, "X\n");
+    } else {
+        strcat(groupAccessRight, "-\n");
+    }
+    if (write(fisierIesire, groupAccessRight, strlen(groupAccessRight)) < 0) {
+        perror("Eroare la scrierea drepturilor de acces grup pentru director!\n");
+        exit(-1);
+    }
+
+    if (dirStat.st_mode & S_IROTH) {
+        strcat(otherAccessRight, "R");
+    } else {
+        strcat(otherAccessRight, "-");
+    }
+    if (dirStat.st_mode & S_IWOTH) {
+        strcat(otherAccessRight, "W");
+    } else {
+        strcat(otherAccessRight, "-");
+    }
+    if (dirStat.st_mode & S_IXOTH) {
+        strcat(otherAccessRight, "X\n\n");
+    } else {
+        strcat(otherAccessRight, "-\n\n");
+    }
+    if (write(fisierIesire, otherAccessRight, strlen(otherAccessRight)) < 0) {
+        perror("Eroare la scrierea drepturilor de acces altii pentru director!\n");
+        exit(-1);
+    }
+}
+
+void parcurgeDirector(char *director, int *fisierIesire) {
+    DIR *dir;
+    struct dirent *intrare;
+
+    if ((dir = opendir(director)) == NULL) {
+        perror("Eroare la deschiderea directorului!\n");
+        exit(-1);
+    }
+    printf("Director deschis: %s\n", director);
     *fisierIesire = creareFisier("statistica.txt");
 
     while ((intrare = readdir(dir)) != NULL) {
+        if (strcmp(intrare->d_name, ".") == 0 || strcmp(intrare->d_name, "..") == 0) {
+            continue;
+        }
+
         char buffer[BUFFER_SIZE];
         sprintf(buffer, "%s/%s", director, intrare->d_name);
         printf("Element gasit: %s\n", buffer);
 
         struct stat fileStat;
-        if (stat(buffer, &fileStat) < 0) {
+        if (lstat(buffer, &fileStat) < 0) {
             perror("Eroare la preluarea informatiilor despre element!\n");
             exit(-1);
         }
 
-        if (S_ISREG(fileStat.st_mode)){
-            if(verificareBMP(buffer)){
+        if (S_ISLNK(fileStat.st_mode)) {
+            char target[BUFFER_SIZE];
+            int octetiCititi;
+            if ((octetiCititi = readlink(buffer, target, sizeof(target) - 1)) != -1) {
+                target[octetiCititi] = '\0';
+                printf("Legatura simbolica: %s -> %s\n", buffer, target);
+                scrieLegaturaSimbolica(*fisierIesire, buffer, target, director);
+            } else {
+                perror("Eroare la citirea legaturii simbolice!\n");
+                exit(-1);
+            }
+        } else if (S_ISREG(fileStat.st_mode)) {
+            if (verificareBMP(buffer)) {
                 char caleCompleta[BUFFER_SIZE];
                 sprintf(caleCompleta, "%s/%s", director, intrare->d_name);
                 int fisierBMP = open(caleCompleta, O_RDONLY);
@@ -294,13 +474,11 @@ void parcurgeDirector(char * director, int *fisierIesire){
                 scriereDataBMP(fisierBMP, *fisierIesire);
                 scriereDateFisier(fisierBMP, *fisierIesire);
                 close(fisierBMP);
-            }
-            else
-            {
+            } else {
                 char caleCompleta[BUFFER_SIZE];
                 sprintf(caleCompleta, "%s/%s", director, intrare->d_name);
                 int fisierSimplu = open(caleCompleta, O_RDONLY);
-                if(fisierSimplu < 0){
+                if (fisierSimplu < 0) {
                     perror("Eroare la deschiderea fisierului simplu!\n");
                     exit(-1);
                 }
@@ -308,23 +486,23 @@ void parcurgeDirector(char * director, int *fisierIesire){
                 scriereDateFisier(fisierSimplu, *fisierIesire);
                 close(fisierSimplu);
             }
+        } else if (S_ISDIR(fileStat.st_mode)) {
+            printf("Este director: %s\n", buffer);
+            scrieDirector(*fisierIesire, buffer);
         }
+   
     }
-    close(*fisierIesire);
-    closedir(dir);
 }
 
+
 int main(int argc, char *argv[]){
-    int fIn = 0, fOut = 0, rd, wr;
-    char *fileName = malloc(BUFFER_SIZE * sizeof(char));
+    int fOut = 0;
 
     if(verificareArgument(argc, argv[1]) == 1){
         parcurgeDirector(argv[1], &fOut);
     }
 
-    close(fIn);
     close(fOut);
-    free(fileName);
 
     return 0;
 }
